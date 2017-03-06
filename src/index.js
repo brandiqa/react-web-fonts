ac591637cccff6ae48d70bb71f4ce126bda5d88d
Michael Wanyoike <micwanyoike@gmail.com>
2017-03-06 17:24:17 +0300

    Implemented web font loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
 import App from './App';
 import './index.css';
+
+WebFont.load({
+  google: {
+    families: ['Titillium Web:300,400,700', 'sans-serif']
+  }
+});
 
 ReactDOM.render(
   <App />,
